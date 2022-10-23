d48699b671401dddbba69a9c22ba833838dcf023
Murat Bayraktar <bayraktar.murat@metu.edu.tr>
2022-10-23 18:55:05 +0300

    demo init

--- run_demo.py
+++ run_demo.py
@@ -9,6 +9,13 @@ from lib.utils import load_graph_data
 from model.dcrnn_supervisor import DCRNNSupervisor
 
 
+# - (DTM) **rosy-morning-93:** data/model/dcrnn_DR_2_h_12_64-64_lr_0.01_bs_64_1016200818/models-1.0771-33060
+# - (BASE) **ancient-terrain-91**: data/model/dcrnn_DR_2_h_12_64-64_lr_0.01_bs_64_1016132112/models-1.6182-30210
+
+DTM_MODEL = 'data/model/dcrnn_DR_2_h_12_64-64_lr_0.01_bs_64_1016200818/config_57.yaml'  # models-1.0771-33060
+BASE_MODEL = 'data/model/dcrnn_DR_2_h_12_64-64_lr_0.01_bs_64_1016132112/config_52.yaml' # models-1.6182-30210
+
+
 def run_dcrnn(args):
     with open(args.config_filename) as f:
         config = yaml.load(f)
@@ -30,7 +37,7 @@ if __name__ == '__main__':
     sys.path.append(os.getcwd())
     parser = argparse.ArgumentParser()
     parser.add_argument('--use_cpu_only', default=False, type=str, help='Whether to run tensorflow on cpu.')
-    parser.add_argument('--config_filename', default='data/model/pretrained/METR-LA/config.yaml', type=str,
+    parser.add_argument('--config_filename', default=DTM_MODEL, type=str,
                         help='Config file for pretrained model.')
     parser.add_argument('--output_filename', default='data/dcrnn_predictions.npz')
     args = parser.parse_args()
